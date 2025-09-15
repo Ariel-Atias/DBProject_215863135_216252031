0b48bbb1edd41df2c562c0b01657a60ca4406b2b
Mvrtry <maoryou3@gmail.com>
2025-09-15 14:31:55 +0300

    Update Constraints.sql

--- Stage_2/Constraints.sql
+++ Stage_2/Constraints.sql
@@ -6,8 +6,9 @@ CREATE INDEX idx_transaction_currency ON Transaction(Currency);
 CREATE INDEX idx_customer_name ON Customer(Name);
 
 -- אילוץ: סכום עסקה לא יכול להיות שלילי
-ALTER TABLE Transaction
-ADD CONSTRAINT chk_amount_positive CHECK (Amount >= 0);
+INSERT INTO Transaction (TransactionID, Amount, Currency, Status, TransactionDate, CustomerID, MerchantID, PaymentMethodID)
+VALUES ('TXN1000', -999.99, 'USD', 'Pending', CURRENT_DATE, 'CUST001', 'MERCH001', 'PAY001');
+
 
 -- אילוץ: סטטוס עסקה רק מתוך רשימה
 ALTER TABLE Transaction
